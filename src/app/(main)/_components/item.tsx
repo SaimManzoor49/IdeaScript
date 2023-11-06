78b8da386b9b941a936e9e8a9338a3702d52b742
unknown <saummanzoor49@gmail.com>
2023-11-06 22:23:37 +0500

    creating-note-from-sidebar

--- src/app/(main)/_components/item.tsx
+++ src/app/(main)/_components/item.tsx
@@ -1,9 +1,13 @@
 'use client'
 import React from 'react'
 import { Id } from '../../../../convex/_generated/dataModel';
-import { LucideIcon, ChevronDown, ChevronRight } from 'lucide-react';
+import { LucideIcon, ChevronDown, ChevronRight, Plus } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import { Skeleton } from '@/components/ui/skeleton';
+import { useMutation } from 'convex/react';
+import { api } from '../../../../convex/_generated/api';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
 
 interface IItemProps {
     id?: Id<'documents'>;
@@ -19,13 +23,44 @@ interface IItemProps {
 }
 
 export default function Item({ label, onClick, icon: Icon, id, active, documentIcon, isSearch, level = 0, onExpand, expanded }: IItemProps) {
+
+    const create= useMutation(api.documents.create) 
+    const router = useRouter()
+
+    const handleExpand = (e:React.MouseEvent<HTMLDivElement,MouseEvent>)=>{
+        e.stopPropagation();
+        onExpand?.();
+    }
+
+    const onCreate = (
+        e:React.MouseEvent<HTMLDivElement,MouseEvent>
+    )=>{
+        e.stopPropagation();
+        if(!id) return;
+
+        const promise = create({
+            title:"Untitled",parentDocument:id
+        }).then((documnetId)=>{
+            if(!expanded){
+                onExpand?.();
+            }
+            // router.push(`/documents/${documnetId}`)
+        })
+
+        toast.promise(promise,{
+            loading:'Creating a new note...',
+            success:'New note created!',
+            error:'Failed to create a new note',
+        })
+    }
+
     const ChavronIcon = expanded ? ChevronDown : ChevronRight
     return (
         <div onClick={onClick} role='button' style={{ paddingLeft: level ? `${(level * 12) + 12}px` : '12px' }} className={cn(
             'group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium',
             active && 'bg-primary/5 text-primary'
         )}>
-            {!!id && (<div role='button' className='h-full rounded-sm hover:bg-neutral-600 mr-1' onClick={()=>{}}>
+            {!!id && (<div role='button' className='h-full rounded-sm hover:bg-neutral-600 mr-1' onClick={handleExpand}>
                 <ChavronIcon className='h-4 w-4 shrink-0 text-muted-foreground/50' />
                 </div>)}
                 {documentIcon?(<div className='shrink-0 mr-2 text-[18px]'>{documentIcon}</div>):(
@@ -41,6 +76,18 @@ export default function Item({ label, onClick, icon: Icon, id, active, documentI
                 ⌘
                 </span>K
             </kbd>)}
+            {!!id&&(
+                <div className='ml-auto flex items-center gap-x-2'>
+                    <div
+                    role='button' 
+                    onClick={onCreate}
+                    className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+                    >
+                        <Plus className='h-4 w-4 text-muted-foreground' />
+                    </div>
+
+                </div>
+            )}
         </div>
     )
 }
