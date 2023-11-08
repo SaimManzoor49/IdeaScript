1328ffa7a8b0af443d15002052aac2b7ad888232
unknown <saummanzoor49@gmail.com>
2023-11-08 23:51:06 +0500

    update-api+title-ui

--- src/app/(main)/_components/navigation.tsx
+++ src/app/(main)/_components/navigation.tsx
@@ -1,6 +1,6 @@
 import { cn } from '@/lib/utils'
 import { ChevronLeftIcon, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from 'lucide-react'
-import { usePathname } from 'next/navigation'
+import { useParams, usePathname } from 'next/navigation'
 import React, { ElementRef, useEffect, useRef, useState } from 'react'
 import { useMediaQuery } from 'usehooks-ts'
 import UserItem from './UserItem'
@@ -18,11 +18,13 @@ import TrashBox from './TrashBox'
 
 import { useSearch } from '../../../../hooks/use-search'
 import { useSettings } from '../../../../hooks/use-settings'
+import Navbar from './Navbar'
 
 
 
 export default function Navigation() {
     const pathname = usePathname()
+    const params = useParams()
     const isMobile = useMediaQuery("(max-width:768px)")
 
     const create = useMutation(api.documents.create)
@@ -36,79 +38,79 @@ export default function Navigation() {
     const search = useSearch();
     const settings = useSettings();
 
-    useEffect(()=>{
-        if(isMobile){
+    useEffect(() => {
+        if (isMobile) {
             collapse()
-        }else{
+        } else {
             resetWith()
         }
-    },[isMobile])
+    }, [isMobile])
 
-    useEffect(()=>{
-        if(isMobile){
+    useEffect(() => {
+        if (isMobile) {
             collapse()
         }
-    },[pathname,isMobile])
+    }, [pathname, isMobile])
 
 
-    const handleMouseDown = (e:React.MouseEvent<HTMLDivElement, MouseEvent>)=>{
+    const handleMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         e.preventDefault();
         e.stopPropagation()
         isResizingRef.current = true
-        document.addEventListener("mousemove" , handleMouseMove)
-        document.addEventListener("mouseup" , handleMouseUp)
+        document.addEventListener("mousemove", handleMouseMove)
+        document.addEventListener("mouseup", handleMouseUp)
     }
 
-    const handleMouseMove = (e:MouseEvent)=>{
-        if(!isResizingRef.current) return;
+    const handleMouseMove = (e: MouseEvent) => {
+        if (!isResizingRef.current) return;
         let newWidth = e.clientX;
-        if(newWidth<240) newWidth = 240;
-        if(newWidth>480) newWidth = 480;
-        if(sideBarRef.current && navBarRef.current){
+        if (newWidth < 240) newWidth = 240;
+        if (newWidth > 480) newWidth = 480;
+        if (sideBarRef.current && navBarRef.current) {
             sideBarRef.current.style.width = `${newWidth}px`
-            navBarRef.current.style.setProperty('left',`${newWidth}px`);
-            navBarRef.current.style.setProperty('width',`calc(100% - ${newWidth}px)`);
+            navBarRef.current.style.setProperty('left', `${newWidth}px`);
+            navBarRef.current.style.setProperty('width', `calc(100% - ${newWidth}px)`);
         }
     }
 
-    const handleMouseUp = ()=>{
+    const handleMouseUp = () => {
         isResizingRef.current = false;
-        document.removeEventListener('mousemove',handleMouseMove)
-        document.removeEventListener('mouseup',handleMouseUp)
+        document.removeEventListener('mousemove', handleMouseMove)
+        document.removeEventListener('mouseup', handleMouseUp)
     }
 
-    const resetWith = ()=>{
-        if(sideBarRef.current&& navBarRef.current){
+    const resetWith = () => {
+        if (sideBarRef.current && navBarRef.current) {
             setIsCollasped(false)
             setIsResetting(true);
 
-            sideBarRef.current.style.width  = isMobile ? "100%":'240px'
-            navBarRef.current.style.setProperty('width', isMobile ? '0':"calc(100% - 240px)");
-            navBarRef.current.style.setProperty('left',isMobile?'100%':'240px')
-            setTimeout(()=>setIsResetting(false),300)
+            sideBarRef.current.style.width = isMobile ? "100%" : '240px'
+            navBarRef.current.style.setProperty('width', isMobile ? '0' : "calc(100% - 240px)");
+            navBarRef.current.style.setProperty('left', isMobile ? '100%' : '240px')
+            setTimeout(() => setIsResetting(false), 300)
         }
     }
 
 
-    const collapse = ()=>{
-        if(sideBarRef.current&& navBarRef.current){
+    const collapse = () => {
+        if (sideBarRef.current && navBarRef.current) {
             setIsCollasped(true)
             setIsResetting(true)
 
             sideBarRef.current.style.width = '0';
-            navBarRef.current.style.setProperty('width','100%')
-            navBarRef.current.style.setProperty('left','0')
-            setTimeout(()=>setIsResetting(false),300)
+            navBarRef.current.style.setProperty('width', '100%')
+            navBarRef.current.style.setProperty('left', '0')
+            setTimeout(() => setIsResetting(false), 300)
         }
     }
 
-    const handleCreate =()=>{
-        const promise = create({title:'Untitled'})
+    const handleCreate = () => {
+        const promise = create({ title: 'Untitled' })
 
-        toast.promise(promise,{
-            loading:"creating a new note...",
-            success:"new note created",
-            error:"Faild to create a new note :(",
+        toast.promise(promise, {
+            loading: "creating a new note...",
+            success: "new note created",
+            error: "Faild to create a new note :(",
         })
     }
 
@@ -121,7 +123,7 @@ export default function Navigation() {
             )}>
 
                 <div
-                    role='button' 
+                    role='button'
                     onClick={collapse}
                     className={cn(
                         'h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition',
@@ -132,35 +134,35 @@ export default function Navigation() {
 
                 <div className="">
                     <UserItem />
-                    <Item 
-                    label='Search'
-                    icon={Search}
-                    isSearch
-                    onClick={search.onOpen}
+                    <Item
+                        label='Search'
+                        icon={Search}
+                        isSearch
+                        onClick={search.onOpen}
                     />
-                    <Item 
-                    label='Setting'
-                    icon={Settings}
-                    onClick={settings.onOpen}
+                    <Item
+                        label='Setting'
+                        icon={Settings}
+                        onClick={settings.onOpen}
                     />
-                    <Item onClick={handleCreate} label={'New page'} icon={PlusCircle}/>
+                    <Item onClick={handleCreate} label={'New page'} icon={PlusCircle} />
                 </div>
                 <div className="mt-4">
-                  <DocumentList />
-                  <Item onClick={handleCreate} icon={Plus} label='Add a page' />
-                  <Popover>
-                    <PopoverTrigger className='w-full mt-4'>
-                        <Item label='Trash' icon={Trash} />
-                    </PopoverTrigger>
-                    <PopoverContent side={isMobile?"bottom":"right"} className='p-0 w-72'>
-                        <TrashBox />
-                    </PopoverContent>
-                  </Popover>
+                    <DocumentList />
+                    <Item onClick={handleCreate} icon={Plus} label='Add a page' />
+                    <Popover>
+                        <PopoverTrigger className='w-full mt-4'>
+                            <Item label='Trash' icon={Trash} />
+                        </PopoverTrigger>
+                        <PopoverContent side={isMobile ? "bottom" : "right"} className='p-0 w-72'>
+                            <TrashBox />
+                        </PopoverContent>
+                    </Popover>
                 </div>
                 <div
-                onMouseDown={handleMouseDown}
-                onClick={resetWith}
-                className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
+                    onMouseDown={handleMouseDown}
+                    onClick={resetWith}
+                    className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
             </aside>
 
             <div
@@ -171,11 +173,19 @@ export default function Navigation() {
                     isMobile && 'left-0 w-full'
                 )}>
 
-                <nav className='bg-transparent px-3 py-2 w-full'>
+                {!!params.documentId
+                    ? (
+                        <Navbar
+                        isCollasped={isCollasped}
+                        onResetWidth={resetWith}
+                        />
+                    ) : (
+                        <nav className='bg-transparent px-3 py-2 w-full'>
 
-                    {isCollasped && <MenuIcon onClick={resetWith} className='h-6 w-6 text-muted-foreground' role='button' />}
+                            {isCollasped && <MenuIcon onClick={resetWith} className='h-6 w-6 text-muted-foreground' role='button' />}
 
-                </nav>
+                        </nav>
+                    )}
 
             </div>
 
