2e8e258c2450b07a73f63c7b24291efe1f4f3087
unknown <saummanzoor49@gmail.com>
2023-11-09 19:08:06 +0500

    Banner-in-trash

--- src/app/(main)/_components/Navbar.tsx
+++ src/app/(main)/_components/Navbar.tsx
@@ -8,6 +8,8 @@ import { api } from '../../../../convex/_generated/api';
 import { Id } from '../../../../convex/_generated/dataModel';
 import { MenuIcon } from 'lucide-react';
 import Title from './Title';
+import Banner from './Banner';
+import Menu from './Menu';
 
 
 
@@ -25,8 +27,11 @@ export default function Navbar({ isCollasped, onResetWidth }: INavbarProps) {
     })
 
     if (document === undefined) {
-        return <nav className='bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center'>
+        return <nav className='bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center justify-between'>
             <Title.Skeleton />
+            <div className="flex items-center gap-x-2">
+                <Menu.Skeleton />
+            </div>
         </nav>
     }
 
@@ -49,9 +54,15 @@ export default function Navbar({ isCollasped, onResetWidth }: INavbarProps) {
 
                 <div className="flex items-center justify-between w-full">
                     <Title initialData={document} />
+                    <div className="flex items-center gap-x-2">
+                        <Menu documentId={document._id} />
+                    </div>
                 </div>
 
             </nav>
+            {document.isArchived && (
+                <Banner documentId={document._id} />
+            ) }
         </>
     )
 }
